8da4f53089106be68a64706f047fe25f6c784582
aevi1103 <aevirontos@gmail.com>
2020-02-28 13:45:08 -0500

    added no cache

--- src/API.js
+++ src/API.js
@@ -6,6 +6,8 @@ let url = 'http://10.129.224.149:81/api/';
 export default axios.create({
     baseURL: url,
     headers: {
-        'Accept': 'application/json'
+        'Accept': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Cache-Control': 'no-cache'
     }
 });
