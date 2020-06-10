5c1222bf5cb94673c5b75cdc22adf1eaff8fc36f
aevi1103 <aevirontos@gmail.com>
2020-06-10 12:44:23 -0400

    added initial test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor} from './redux/store';
+
+import './index.css';
 import App from './App';
+
+
+import 'tachyons'
+import './sass/main.scss'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<BrowserRouter>
+                      <PersistGate loading={null} persistor={persistor}>
+                          <Provider store={store}>
+                              <App />
+                          </Provider>
+                      </PersistGate>
+                  </BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
