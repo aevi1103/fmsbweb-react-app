bfaa741ad3dd7e6b382342814c2090e3d33407ab
Aevi <aevirontos@gmail.com>
2020-11-25 08:39:57 -0500

    added oee lines page

---
+++ src/pages/oee/groups-page.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import api from '../../API'
+import {
+    PageHeader,
+    Layout,
+    Card,
+    Input,
+    Button,
+    Row,
+    Col,
+    Form
+} from 'antd'
+
+const { Content } = Layout;
+
+const Groups = () => {
+
+
+    return (
+        <>
+            <PageHeader title="Lines" className="site-page-header" />
+
+            <Content>
+
+
+            </Content>
+        
+        </>
+    )
+}
