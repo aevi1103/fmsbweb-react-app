24170ff31ce898bbb258f859151dcb37dde52bd6
Aevi <aevirontos@gmail.com>
2021-01-15 13:51:35 -0500

    add days in day on hand component

--- src/containers/logistics-dashboard/components/days-on-hand-table/days-on-hand-table.component.jsx
+++ src/containers/logistics-dashboard/components/days-on-hand-table/days-on-hand-table.component.jsx
@@ -1,22 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import moment from 'moment';
 import numeral from 'numeral';
 import Highlighter from 'react-highlight-words';
+import { EditOutlined } from '@ant-design/icons';
+import api from '../../../../core/utilities/api'
 
 import { numberSorter } from '../../../../core/utilities/helpers';
 import { SearchOutlined } from '@ant-design/icons';
-import { Table, Input, Button } from "antd";
+import { 
+  Table,
+  Input,
+  Button,
+  Tooltip,
+  Modal,
+  Form,
+  message
+} from "antd";
 
 import { getColorCode, getColorRowClass } from '../../service/helper'
 
 const DaysOnHandTable = () => {
 
+  let searchInput = null;
   const loading = useSelector(({ logistics: { isStockStatusFetching } }) => isStockStatusFetching) || false;
   const daysOnHand = useSelector(({ logistics: { stockStatusCollection } }) => stockStatusCollection.daysOnHand) || [];
 
+  const [form] = Form.useForm();
+  const [dataSource, setDataSource] = useState([])
   const [searchText, setSearchText] = useState('');
   const [searchedColumn, setSearchedColumn] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedRecord, setSelectedRecord] = useState(null);
+
+  useEffect(() => {
+
+      const ds = daysOnHand.map((rowData, i) => {
+
+            const {
+                date,
+                program,
+                material,
+                qty,
+                daysOnHand,
+                safetyStock,
+                safetyDays
+            } = rowData;
+
+            return {
+                key: i,
+                ...rowData,
+                date: moment(date).format('MM/DD/YYYY'),
+                prog: program,
+                mat: material,
+                stock: numeral(qty).format('0,0'),
+                safetyStock: numeral(safetyStock).format('0,0'),
+                safetyStockQty: safetyStock,
+                safetyDays: safetyDays?.days ?? 0,
+                stockSafetyDaysId: safetyDays?.stockSafetyDaysId ?? 0,
+                doh: numeral(daysOnHand).format('0.00'),
+                bgColor: getColorCode(daysOnHand, safetyStock)
+            }
+      })
+
+      setDataSource(ds)
+
+  }, [daysOnHand])
+
+  useEffect(() => {
+
+    console.log(selectedRecord)
+
+
+  }, [selectedRecord])
 
   const handleSearch = (selectedKeys, confirm, dataIndex) => {
       confirm();
@@ -29,7 +85,56 @@ const DaysOnHandTable = () => {
        setSearchText('');
    };
 
-   let searchInput = null;
+  const onEdit = record => {
+
+    form.resetFields()
+
+    setModalVisible(true)
+    setSelectedRecord(record)
+
+    form.setFieldsValue({
+      days: record?.safetyDays
+    })
+    
+  }
+
+  const onFinish = ({ days }) => {
+  
+    try {
+      
+      const { stockSafetyDaysId, material, safetyStockQty, qty } = selectedRecord || {};
+
+      api.post(`/logistics/days`, {
+        stockSafetyDaysId: stockSafetyDaysId,
+        materialNumber: material,
+        days: +days
+      })
+
+      message.success(`Successfully submitted!`)
+      setModalVisible(false)
+
+      const safetyStockOverDays = days === 0 ? 0 : safetyStockQty / days;
+      const daysOnHand = safetyStockOverDays === 0 ? 0 : qty / safetyStockOverDays;
+
+      const rowObject = {
+        ...selectedRecord,
+        safetyDays: days,
+        daysOnHand,
+        doh: numeral(daysOnHand).format('0.00')
+      }
+
+      const newDataSource = [...dataSource.filter(({ material }) => material !== selectedRecord?.material), rowObject]
+      const sortDataSource = newDataSource.sort((a, b) => b.qty - a.qty)
+
+      setDataSource(sortDataSource)
+
+    } catch (error) {
+      message.error(`Something went wrong!`)
+    }
+
+  }
+
+  const onSubmit = () => form.submit()
 
    const getColumnSearchProps = dataIndex => ({
        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
@@ -88,114 +193,115 @@ const DaysOnHandTable = () => {
        {
            title: 'Date',
            dataIndex: 'date',
-           sorter: (a, b) => new Date(a.date) - new Date(b.date),
-           sortDirections: ['descend', 'ascend']
+          //  sorter: (a, b) => new Date(a.date) - new Date(b.date),
+          //  sortDirections: ['descend', 'ascend']
        },
        {
            title: 'Program',
            dataIndex: 'prog',
-           sorter: (a, b) => a.prog.localeCompare(b.prog),
-           sortDirections: ['descend', 'ascend'],
+          //  sorter: (a, b) => a.prog.localeCompare(b.prog),
+          //  sortDirections: ['descend', 'ascend'],
            ...getColumnSearchProps('prog')
        },
        {
            title: 'Material',
            dataIndex: 'mat',
-           sorter: (a, b) =>  a.mat.localeCompare(b.mat),
-           sortDirections: ['descend', 'ascend'],
+          //  sorter: (a, b) =>  a.mat.localeCompare(b.mat),
+          //  sortDirections: ['descend', 'ascend'],
            ...getColumnSearchProps('mat')
        },
        {
            title: 'Stock Qty',
            dataIndex: 'stock',
-           sorter: (a, b) => numberSorter(a.stock, b.stock),
-           sortDirections: ['descend', 'ascend'],
-        //    render: (text, record, index) => {
-        //      const { bgColor } = record;
-        //      return <Tag color={bgColor}>{text}</Tag>          
-        //  }
-       },
-       {
+          //  sorter: (a, b) => numberSorter(a.stock, b.stock),
+          //  sortDirections: ['descend', 'ascend']
+      },
+      {
          title: 'Safety Stock',
          dataIndex: 'safetyStock',
-         sorter: (a, b) => numberSorter(a.safetyStock, b.safetyStock),
-         sortDirections: ['descend', 'ascend']
-     },
-       {
+        //  sorter: (a, b) => numberSorter(a.safetyStock, b.safetyStock),
+        //  sortDirections: ['descend', 'ascend']
+      },
+      {
+          title: 'Days',
+          dataIndex: 'safetyDays',
+          // sorter: (a, b) => numberSorter(a.safetyDays.days, b.safetyStock.days),
+          // sortDirections: ['descend', 'ascend'],
+          render: (text, record, index) => {       
+
+              const { safetyDays } = record;
+
+              return (
+                <>
+                  <span className="mr2">{safetyDays}</span> 
+                  <Tooltip title="Edit days">
+                    <span style={{ cursor: 'pointer' }} onClick={() => onEdit(record)} aria-hidden="true" ><EditOutlined /></span>
+                  </Tooltip>
+                </>
+              )
+                
+          }
+      },
+      {
            title: 'Days On Hand',
            dataIndex: 'doh',
-           sorter: (a, b) => numberSorter(a.doh, b.doh),
-           sortDirections: ['descend', 'ascend'],
-          //  render: (text, record, index) => {       
-          //      const { bgColor } = record;
-          //    return <Tag color={bgColor}>{text}</Tag>      
-          //  }
-       }
+          //  sorter: (a, b) => numberSorter(a.doh, b.doh),
+          //  sortDirections: ['descend', 'ascend'],
+      }
      ];
      
-
-
-     const dataSource = daysOnHand
-        // .filter(({ qty, safetyStock }) => qty > 0 && safetyStock > 0)
-        .map((rowData, i) => {
-
-          const {
-              date,
-              program,
-              material,
-              qty,
-              daysOnHand,
-              safetyStock
-          } = rowData;
-
-          return {
-              key: i,
-              date: moment(date).format('MM/DD/YYYY'),
-              prog: program,
-              mat: material,
-              stock: numeral(qty).format('0,0'),
-              safetyStock: numeral(safetyStock).format('0,0'),
-              doh: numeral(daysOnHand).format('0.00'),
-              bgColor: getColorCode(daysOnHand, safetyStock)
-          }
-       
-     })
+     
 
      return (
-       <Table 
-          size="small"
-          loading={loading}
-          columns={columns}
-          dataSource={dataSource}
-          pagination={false}
-          rowClassName={(record, index) => {
+       <>
+          <Table 
+            size="small"
+            loading={loading}
+            columns={columns}
+            dataSource={dataSource}
+            pagination={false}
+            rowClassName={(record, index) => {
 
-            const { doh, safetyStock } = record;
-            const rowClass = getColorRowClass(parseFloat(doh), parseFloat(safetyStock));
+              const { doh, safetyStock } = record;
+              const rowClass = getColorRowClass(parseFloat(doh), parseFloat(safetyStock));
 
-            return `${rowClass}`;
+              return `${rowClass}`;
 
-          }}
-          onRow={(record, rowIndex) => {
+            }}
+            onRow={(record, rowIndex) => {
 
-            const antRowClassName = 'ant-table-row'
+              const antRowClassName = 'ant-table-row'
 
-            
-
-            return {
-              onMouseEnter: e => {
-                if (parseInt(record.safetyStock)  > 0) {
-                  e.target.parentElement.classList.remove(antRowClassName)
-                }
-              },
-              onMouseLeave: e => {
-                if (parseInt(record.safetyStock)  > 0) {
-                  e.target.parentElement.classList.add(antRowClassName)
+              return {
+                onMouseEnter: e => {
+                  if (parseInt(record.safetyStock)  > 0) {
+                    e.target.parentElement.classList.remove(antRowClassName)
+                  }
+                },
+                onMouseLeave: e => {
+                  if (parseInt(record.safetyStock)  > 0) {
+                    e.target.parentElement.classList.add(antRowClassName)
+                  }
                 }
               }
-            }
 
-          }} />     
+            }} />    
+
+          <Modal 
+            title={`Edit ${selectedRecord?.material} Days`} 
+            visible={modalVisible} 
+            onOk={onSubmit} 
+            okText="Submit"
+            onCancel={() => setModalVisible(false)}>
+
+            <Form form={form} onFinish={onFinish}>
+              <Form.Item label="Days" name="days" rules={[{ required: true, message: 'Please enter days' }]}>
+                <Input placeholder="Enter days" />
+              </Form.Item>
+            </Form>
+
+          </Modal>
+       </>
    )
 }
 
